07ebc70a388856eb341fe93f2ac5b6589adee8a4
Melchizedek Stanley <stanexseven@gmail.com>
2025-01-01 09:28:34 +0300

    added form validations

--- app/login.tsx
+++ app/login.tsx
@@ -1,16 +1,38 @@
-import { Text } from "@/components/ui/Text"
-import React, { useEffect, useState } from "react"
-import { SafeAreaView } from "react-native-safe-area-context"
-import { ArrowLeft } from 'lucide-react-native';
-import { View } from "react-native";
-import { Button } from "@/components/ui/Button";
-import { Image } from "expo-image";
-import { Input } from "@/components/ui/Input";
-import { Checkbox } from "@/components/ui/Checkbox";
 import { Link, useRouter } from "expo-router";
-import { KeyboardAwareScrollView } from "react-native-keyboard-aware-scroll-view";
-import { useKeyboardVisibility } from "@/lib/useKeyboard";
+import React, { useEffect, useState } from "react"
+
+import { Image } from "expo-image";
+import { StyleSheet, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context"
 import Animated, { useSharedValue, withSpring } from "react-native-reanimated";
+import { KeyboardAwareScrollView } from "react-native-keyboard-aware-scroll-view";
+
+import { Text } from "@/components/ui/Text"
+import { Input } from "@/components/ui/Input";
+import { Button } from "@/components/ui/Button";
+import { Checkbox } from "@/components/ui/Checkbox";
+
+import { ArrowLeft } from 'lucide-react-native';
+
+import * as yup from "yup"
+import { Formik } from "formik";
+import { useKeyboardVisibility } from "@/lib/useKeyboard";
+
+const loginSchema = yup.object().shape({
+    phoneNumber: yup.string()
+        .trim()
+        .matches(
+            /^(07|06)\d{8}$/,
+            "Your mobile number should start with 07 or 06 followed by 8 other digits."
+        )
+        .min(10, "Mobile number should contain 10 digits.")
+        .max(10, "Mobile number should contain 10 digits.")
+        .required("Please enter your mobile number."),
+    password: yup.string()
+        .trim()
+        .min(4, "Password should be at least 4 characters long.")
+        .required('Please enter your password')
+})
 
 export default function login() {
     const [remember, setRemember] = useState(false)
@@ -39,6 +61,7 @@ export default function login() {
                 extraScrollHeight={200} // Adjust this value as needed
                 showsVerticalScrollIndicator={false}
                 keyboardOpeningTime={0}
+                keyboardShouldPersistTaps="handled"
             >
                 <Button className="bg-primary w-12 h-12 rounded-full p-2 items-center justify-center"
                     android_ripple={{
@@ -62,12 +85,7 @@ export default function login() {
                     >
                         <Image
                             source={img.uri}
-                            style={{
-                                width: 213,
-                                height: 131,
-                                justifyContent: "center",
-                                alignSelf: "center",
-                            }}
+                            style={styles.img}
                             contentFit="cover"
                         />
                     </Animated.View>
@@ -75,54 +93,112 @@ export default function login() {
                     <View className={keyboard ? "mt-4" : "mt-20"}>
                         <Text className="text-4xl font-bold text-center text-primary" style={{ color: "#2B4B80" }}>Login</Text>
                         <View className="py-4" />
-                        <View className="flex flex-col gap-2">
-                            <View className="gap-2">
-                                <Text>Mobile Number</Text>
-                                <Input placeholder="Enter your mobile number" />
-                            </View>
-                            <View className="py-0.5" />
-                            <View className="gap-2">
-                                <Text>Password</Text>
-                                <Input placeholder="Enter your password" />
-                            </View>
-                            <View className="py-0.5" />
-                            <View
-                                style={{
-                                    display: "flex",
-                                    alignItems: "center",
-                                    flexDirection: "row",
-                                    justifyContent: "space-between",
-                                    width: "99%"
-                                }}
-                            >
-                                <View
-                                    style={{
-                                        display: "flex",
-                                        alignItems: "center",
-                                        flexDirection: "row"
-                                    }}
-                                    className="gap-2"
-                                >
-                                    <Checkbox
-                                        checked={remember}
-                                        onCheckedChange={() => setRemember(!remember)}
-                                    />
-                                    <Text>Remember me</Text>
-                                </View>
+                        <Formik
+                            validationSchema={loginSchema}
+                            initialValues={{
+                                phoneNumber: "",
+                                password: ""
+                            }}
+                            onSubmit={(values) => {
+                                console.log(
+                                    values.phoneNumber,
+                                    values.password
+                                );
+                            }}
+                        >
+                            {({ handleChange, handleSubmit, setFieldValue, errors, isValid, values }) => (
+                                <>
+                                    <View className="flex flex-col gap-2">
+                                        <View className="gap-2">
+                                            <Text className={errors.phoneNumber ? "text-destructive" : ""}>
+                                                {errors.phoneNumber ? errors.phoneNumber : "Mobile Number"}
+                                            </Text>
+                                            <Input
+                                                placeholder="Enter your mobile number"
+                                                onChangeText={handleChange('phoneNumber')}
+                                                value={values.phoneNumber}
+                                                className={
+                                                    errors.phoneNumber ?
+                                                        "bg-destructive/10 border-destructive border-2 text-destructive p-3"
+                                                        : ""
+                                                }
+                                                placeholderClassName={errors.phoneNumber ? "text-destructive" : ""}
+                                            />
+                                        </View>
+                                        <View className="py-0.5" />
+                                        <View className="gap-2">
+                                            <Text className={errors.password ? "text-destructive" : ""}>
+                                                {errors.password ? errors.password : "Password"}
+                                            </Text>
+                                            <Input
+                                                placeholder="Enter your password"
+                                                onChangeText={handleChange('password')}
+                                                value={values.password}
+                                                className={
+                                                    errors.password ?
+                                                        "bg-destructive/10 border-destructive border-2 text-destructive p-3"
+                                                        : ""
+                                                }
+                                            />
+                                        </View>
+                                        <View className="py-0.5" />
+                                        <View
+                                            style={styles.options}
+                                        >
+                                            <View
+                                                style={styles.rememberCheckbox}
+                                                className="gap-2"
+                                            >
+                                                <Checkbox
+                                                    checked={remember}
+                                                    onCheckedChange={() => setRemember(!remember)}
+                                                />
+                                                <Text>Remember me</Text>
+                                            </View>
 
-                                <Link href="/" className="text-primary">Forgot Password?</Link>
+                                            <Link href="/" className="text-primary">Forgot Password?</Link>
 
-                            </View>
-                            <View className="py-0.5" />
-                            <Button>Login</Button>
-                            <View className="py-0.5" />
-                            <Text className="mx-auto text-center self-center items-center">
-                                Don't have an account? <Link href="/register" className="text-primary">Register</Link>
-                            </Text>
-                        </View>
+                                        </View>
+                                        <View className="py-0.5" />
+                                        <Button
+                                            onPress={handleSubmit}
+                                            disabled={!isValid}
+                                            className={!isValid ? "opacity-75" : ""}
+                                        >
+                                            Create Account
+                                        </Button>
+                                        <View className="py-0.5" />
+                                        <Text className="mx-auto text-center self-center items-center">
+                                            Don't have an account? <Link href="/register" className="text-primary">Register</Link>
+                                        </Text>
+                                    </View>
+                                </>
+                            )}
+                        </Formik>
                     </View>
                 </View>
             </KeyboardAwareScrollView>
         </SafeAreaView>
     )
 }
+
+const styles = StyleSheet.create({
+    img: {
+        width: 213,
+        height: 131,
+        justifyContent: "center",
+        alignSelf: "center",
+    },
+    options: {
+        display: "flex",
+        alignItems: "center",
+        flexDirection: "row",
+        justifyContent: "space-between",
+        width: "99%"
+    },
+    rememberCheckbox: {
+        display: "flex",
+        alignItems: "center",
+        flexDirection: "row"
+    }
+})
--- app/register.tsx
+++ app/register.tsx
@@ -1,17 +1,38 @@
-import { Text } from "@/components/ui/Text"
 import React, { useEffect, useState } from "react"
-import { SafeAreaView } from "react-native-safe-area-context"
-import { ArrowLeft } from 'lucide-react-native';
-import { View } from "react-native";
-import { Button } from "@/components/ui/Button";
-import { Image } from "expo-image";
-import { Input } from "@/components/ui/Input";
-import { Checkbox } from "@/components/ui/Checkbox";
-import { Link, useRouter } from "expo-router";
 
-import { KeyboardAwareScrollView } from "react-native-keyboard-aware-scroll-view";
-import { useKeyboardVisibility } from "@/lib/useKeyboard";
+import { Image } from "expo-image";
+import { View } from "react-native";
+import { ArrowLeft } from 'lucide-react-native';
+import { SafeAreaView } from "react-native-safe-area-context"
 import Animated, { useSharedValue, withSpring } from "react-native-reanimated";
+import { KeyboardAwareScrollView } from "react-native-keyboard-aware-scroll-view";
+
+import { Text } from "@/components/ui/Text"
+import { Input } from "@/components/ui/Input";
+import { Link, useRouter } from "expo-router";
+import { Button } from "@/components/ui/Button";
+import { Checkbox } from "@/components/ui/Checkbox";
+
+import * as yup from "yup"
+import { Formik } from "formik";
+import { useKeyboardVisibility } from "@/lib/useKeyboard";
+
+const registerSchema = yup.object().shape({
+    name: yup.string().required("Please enter your full name"),
+    phoneNumber: yup.string()
+        .trim()
+        .matches(
+            /^(07|06)\d{8}$/,
+            "Your mobile number should start with 07 or 06 followed by 8 other digits."
+        )
+        .min(10, "Mobile number should contain 10 digits.")
+        .max(10, "Mobile number should contain 10 digits.")
+        .required("Please enter your mobile number."),
+    password: yup.string()
+        .trim()
+        .min(4, "Password should be at least 4 characters long.")
+        .required('Please enter your password')
+})
 
 export default function register() {
     const router = useRouter()
@@ -75,28 +96,89 @@ export default function register() {
                             Create Account
                         </Text>
                         <View className="py-4" />
-                        <View className="flex flex-col gap-2">
-                            <View className="gap-2">
-                                <Text>Your Name</Text>
-                                <Input placeholder="Enter your full name" />
-                            </View>
-                            <View className="gap-2">
-                                <Text>Mobile Number</Text>
-                                <Input placeholder="Enter your mobile number" />
-                            </View>
-                            <View className="py-0.5" />
-                            <View className="gap-2">
-                                <Text>Password</Text>
-                                <Input placeholder="Enter your password" />
-                            </View>
-                            <View className="py-0.5" />
-                            <Button>Register</Button>
-                            <View className="py-0.5" />
-                            <Text className="mx-auto text-center self-center items-center">
-                                Already have and account? <Link href="/login" className="text-primary">Login</Link>
-                            </Text>
-                        </View>
+
+                        <Formik
+                            validationSchema={registerSchema}
+                            initialValues={{
+                                name: "",
+                                phoneNumber: "",
+                                password: ""
+                            }}
+                            onSubmit={values => {
+                                console.log(
+                                    values.name,
+                                    values.phoneNumber,
+                                    values.password
+                                );
+                            }}
+                        >
+                            {({ handleChange, handleSubmit, errors, isValid, values }) => (
+                                <>
+                                    <View className="flex flex-col gap-2">
+                                        <View className="gap-2">
+                                            <Text className={errors.name ? "text-destructive" : ""}>
+                                                {errors.name ? errors.name : "Your full name"}
+                                            </Text>
+                                            <Input
+                                                placeholder="Enter your mobile number"
+                                                onChangeText={handleChange('name')}
+                                                value={values.name}
+                                                className={
+                                                    errors.name ?
+                                                        "bg-destructive/10 border-destructive border-2 text-destructive p-3"
+                                                        : ""
+                                                }
+                                                placeholderClassName={errors.name ? "text-destructive" : ""}
+                                            />
+                                        </View>
+                                        <View className="gap-2">
+                                            <Text className={errors.phoneNumber ? "text-destructive" : ""}>
+                                                {errors.phoneNumber ? errors.phoneNumber : "Mobile Number"}
+                                            </Text>
+                                            <Input
+                                                placeholder="Enter your mobile number"
+                                                onChangeText={handleChange('phoneNumber')}
+                                                value={values.phoneNumber}
+                                                className={
+                                                    errors.phoneNumber ?
+                                                        "bg-destructive/10 border-destructive border-2 text-destructive p-3"
+                                                        : ""
+                                                }
+                                                placeholderClassName={errors.phoneNumber ? "text-destructive" : ""}
+                                            />
+                                        </View>
+                                        <View className="gap-2">
+                                            <Text className={errors.password ? "text-destructive" : ""}>
+                                                {errors.password ? errors.password : "Password"}
+                                            </Text>
+                                            <Input
+                                                placeholder="Enter your password"
+                                                onChangeText={handleChange('password')}
+                                                value={values.password}
+                                                className={
+                                                    errors.password ?
+                                                        "bg-destructive/10 border-destructive border-2 text-destructive p-3" : ""
+                                                }
+                                            />
+                                        </View>
+                                        <View className="py-0.5" />
+                                        <Button
+                                            onPress={handleSubmit}
+                                            disabled={!isValid}
+                                            className={!isValid ? "opacity-75" : ""}
+                                        >
+                                            Login
+                                        </Button>
+                                        <View className="py-0.5" />
+                                        <Text className="mx-auto text-center self-center items-center">
+                                            Don't have an account? <Link href="/register" className="text-primary">Register</Link>
+                                        </Text>
+                                    </View>
+                                </>
+                            )}
+                        </Formik>
                     </View>
+
                 </View>
 
             </KeyboardAwareScrollView>
