786d0b9ad0034f7baef9e3a1540d650a134e8a4c
stanmelchizedek <stanexseven@gmail.com>
2025-01-20 15:53:59 +0300

    edited subscription

--- app/(user)/(settings)/subscription.tsx
+++ app/(user)/(settings)/subscription.tsx
@@ -6,9 +6,14 @@ import BackButton from "@/components/back-button";
 import { Check } from "lucide-react-native";
 import { Checkbox } from "@/components/ui/Checkbox";
 import { RadioGroup, RadioGroupItem } from "@/components/ui/RadioGroup"
+import { Image } from "expo-image"
 
 export default function subscription() {
-  const [value, setValue] = React.useState('Comfortable');
+  const [value, setValue] = React.useState('Standard Plan');
+
+  const img = {
+    uri: require("@/assets/images/illustrations/subscription.svg")
+  }
 
   function onLabelPress(label: string) {
     return () => {
@@ -19,17 +24,33 @@ export default function subscription() {
   return (
     <SafeAreaView className="flex-1 p-4 bg-white">
       <BackButton />
-      <Text variant="huge" className="mt-[4%]">
-        Subscriptions
-      </Text>
 
-      <View className="mt-[4%] flex flex-col gap-4">
-        <RadioGroup value={value} onValueChange={setValue}>
-          <RadioItemCard value="Standard Plan" onLabelPress={onLabelPress('Standard Plan')} />
-          <RadioItemCard value="Premium Plan" onLabelPress={onLabelPress('Premium Plan')} />
-        </RadioGroup>
+      <View className="flex-1 items-center justify-center gap-4">
+        <Image
+          source={img.uri}
+          style={{
+            width: 360 / 2,
+            height: 350 / 2,
+            justifyContent: "center",
+            alignSelf: "center"
+          }}
+          contentFit="cover"
+        />
+        <View>
+          <Text variant="huge" className="mt-[4%] text-center">
+            Subscriptions
+          </Text>
+          <Text className="text-center px-6 mt-2">
+            Choose a plan that helps you manage your homes more {"\n"} easily and effeciently.
+          </Text>
+        </View>
 
-
+        <View className="mt-[4%] flex flex-col gap-4 mb-[4%]">
+          <RadioGroup value={value} onValueChange={setValue}>
+            <RadioItemCard value="Standard Plan" onLabelPress={onLabelPress('Standard Plan')} />
+            <RadioItemCard value="Premium Plan" onLabelPress={onLabelPress('Premium Plan')} />
+          </RadioGroup>
+        </View>
 
       </View>
     </SafeAreaView>
@@ -45,7 +66,7 @@ function RadioItemCard({
 }) {
   return (
     <Pressable onPress={onLabelPress}>
-      <View className="bg-primary/10 rounded-lg p-4 border-2 border-primary gap-4 relative">
+      <View className="bg-primary/10 rounded-xl p-4 border-2 border-primary gap-4 relative">
         <View className="flex flex-row items-center justify-between gap-4 w-full">
           <Text className="font-semibold text-xl text-primary">{value}</Text>
           <View>
